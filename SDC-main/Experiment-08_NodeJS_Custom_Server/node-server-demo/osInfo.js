17d201d3b5fa94424daeea666bfa3ebb7e158b2b
Pokala.Srilakshmi <psrilakshmi375@gmail.com>
2025-10-04 21:24:27 +0530

    Delete SDC-main directory

--- SDC-main/Experiment-08_NodeJS_Custom_Server/node-server-demo/osInfo.js
+++
@@ -1,9 +0,0 @@
-const os = require('os');
-
-console.log('OS Info:');
-console.log('Platform:', os.platform());
-console.log('CPU Architecture:', os.arch());
-console.log('Total Memory:', os.totalmem());
-console.log('Free Memory:', os.freemem());
-console.log('Uptime (seconds):', os.uptime());
-console.log('User Info:', os.userInfo());
--- SDC-main/Experiment-16_DJango_Todo-app/django_todo_app/todo/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('delete/<int:task_id>/', views.delete_task, name='delete_task'),
-]
